8b70dc4248660fa1dfa34ecd6b37805477f23c15
tortorse <tortorse@gmail.com>
2018-06-22 11:47:46 +0800

    fix:remove at  symbol

--- src/components/LocaleProvider/en_US.js
+++ src/components/LocaleProvider/en_US.js
@@ -1,9 +1,9 @@
-import InputItem from '@/components/InputItem/locale/en_US'
-import Pagination from '@/components/Pagination/locale/en_US'
-import DatePicker from '@/components/DatePicker/locale/en_US'
-import DatePickerView from '@/components/MobileDatePicker/locale/en_US'
-import Picker from '@/components/Picker/locale/en_US'
-import SearchBar from '@/components/SearchBar/locale/en_US'
+import InputItem from '../InputItem/locale/en_US'
+import Pagination from '../Pagination/locale/en_US'
+import DatePicker from '../DatePicker/locale/en_US'
+import DatePickerView from '../MobileDatePicker/locale/en_US'
+import Picker from '../Picker/locale/en_US'
+import SearchBar from '../SearchBar/locale/en_US'
 
 export default {
   locale: 'en',
