9e51a610872cbd5283fab45555e1dd105403504d
QueenieMar <qianethel@qq.com>
2018-06-26 14:21:22 +0800

    chore: karma config file

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,31 +3,32 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
-// var webpackConfig = require('../../build/webpack.test.conf')
+var webpackConfig = require('../../build/webpack.test.conf')
 
-// module.exports = function (config) {
-//   config.set({
-//     // to run in additional browsers:
-//     // 1. install corresponding karma launcher
-//     //    http://karma-runner.github.io/0.13/config/browsers.html
-//     // 2. add it to the `browsers` array below.
-//     browsers: ['Chrome'],
-//     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
-//     reporters: ['spec', 'coverage'],
-//     files: ['./specs/*.spec.js'],
-//     preprocessors: {
-//       './specs/*.spec.js': ['webpack', 'sourcemap']
-//     },
-//     webpack: webpackConfig,
-//     webpackMiddleware: {
-//       noInfo: true
-//     },
-//     coverageReporter: {
-//       dir: './coverage',
-//       reporters: [
-//         { type: 'lcov', subdir: '.' },
-//         { type: 'text-summary' }
-//       ]
-//     }
-//   })
-// }
+module.exports = function (config) {
+  // config.set({
+  //   // to run in additional browsers:
+  //   // 1. install corresponding karma launcher
+  //   //    http://karma-runner.github.io/0.13/config/browsers.html
+  //   // 2. add it to the `browsers` array below.
+  //   browsers: ['Chrome'],
+  //   frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+  //   reporters: ['spec', 'coverage'],
+  //   files: ['./specs/*.spec.js'],
+  //   preprocessors: {
+  //     './specs/*.spec.js': ['webpack', 'sourcemap']
+  //   },
+  //   webpack: webpackConfig,
+  //   webpackMiddleware: {
+  //     noInfo: true
+  //   },
+  //   coverageReporter: {
+  //     dir: './coverage',
+  //     reporters: [
+  //       { type: 'lcov', subdir: '.' },
+  //       { type: 'text-summary' }
+  //     ]
+  //   }
+  // })
+  console.log('karma config file is empty!')
+}
