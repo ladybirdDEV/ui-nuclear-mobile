ad8f0574ed1d590d064235f4cbf1615d3dfb7ae0
QueenieMar <qianethel@qq.com>
2018-06-27 17:28:25 +0800

    chore: add more echo info

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
 # 保证正确的vuepress依赖，用yarn重新安装一遍vuepress
 yarn add vuepress@^0.8.4
 
-echo "Starting to build docs\n"
+echo "Starting to build docs************"
 
 npm run docs:build
 
@@ -14,9 +14,12 @@ cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
-echo "Starting to update gh-pages\n"
+echo "Starting to update gh-pages************"
+echo "Starting to git-init************"
 git init
+echo "Starting to git-add************"
 git add -A
+echo "Starting to git-commit************"
 git commit -m 'chore: deployed site by Travis CI'
 
 # 如果发布到 https://<USERNAME>.github.io
@@ -25,7 +28,7 @@ git commit -m 'chore: deployed site by Travis CI'
 git push -f git@github.com:ladybirdDEV/ui-nuclear-mobile.git master:gh-pages
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-echo "Done! \n"
+echo "Done!************"
 else
  echo "Skipped updating gh-pages, because build is not triggered from the master branch."
 fi;
