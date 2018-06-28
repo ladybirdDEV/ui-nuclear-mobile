dbded7b858b4b2250e71d8a34d58a3547d59f3c8
QueenieMar <qianethel@qq.com>
2018-06-28 14:47:06 +0800

    fix: remove 'DrawerInline' in sidebar 'cause it does't hava a component named 'DrawerInline'

--- docs/.vuepress/components/sidebar.js
+++ docs/.vuepress/components/sidebar.js
@@ -1,5 +1,5 @@
 let layoutBar = ['Flex', 'WhiteSpace', 'WingBlank']
-let navBar = ['Drawer', 'DrawerInline', 'Menu', 'NavBar', 'Pagination', 'Popover', 'SegmentedControl', 'TabBar', 'Tabs']
+let navBar = ['Drawer', 'Menu', 'NavBar', 'Pagination', 'Popover', 'SegmentedControl', 'TabBar', 'Tabs']
 let dataEntryBar = ['Button', 'Calendar', 'Checkbox', 'DatePicker', 'DatePickerView', 'ImagePicker', 'InputItem', 'Picker', 'PickerView', 'Radio', 'Range', 'SearchBar', 'Slider', 'Stepper', 'Switch', 'TextareaItem']
 let dataDisBar = ['Accordion', 'Badge', 'Card', 'Carousel', 'Grid', 'Icon', 'List', 'NoticeBar', 'Steps', 'Tag']
 let feedbackBar = ['ActionSheet', 'ActivityIndicator', 'Modal', 'Progress', 'Toast']
