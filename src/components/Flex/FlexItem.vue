e41d75f3f080d1530c6470e4edb76eb950499747
tortorse <tortorse@gmail.com>
2018-06-28 14:21:40 +0800

    fix: move style to less for flex, and remove flex-item margin and min-width

--- src/components/Flex/FlexItem.vue
+++ src/components/Flex/FlexItem.vue
@@ -15,15 +15,3 @@ export default {
   }
 }
 </script>
-<style>
-.um-flexbox .um-flexbox-item {
-  box-sizing: border-box;
-  flex: 1;
-  margin-left: 8px;
-  min-width: 10px;
-}
-
-.um-flexbox .um-flexbox-item:first-child {
-  margin-left: 0;
-}
-</style>
