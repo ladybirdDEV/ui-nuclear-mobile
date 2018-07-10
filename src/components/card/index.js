7391bbc337304256d1bc3942a744dabb1ba4754b
QueenieMar <qianethel@qq.com>
2018-07-10 17:07:35 +0800

    fix: card card-header card-body card-footer

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,4 +2,7 @@ import Card from './card'
 import CardHeader from './card-header'
 import CardBody from './card-body'
 import CardFooter from './card-footer'
-export { Card, CardHeader, CardBody, CardFooter }
+Card.Header = CardHeader
+Card.Body = CardBody
+Card.Footer = CardFooter
+export default Card
