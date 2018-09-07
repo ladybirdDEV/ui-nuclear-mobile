604059c045f42d2a0f4f26fba42f5df67b7f8ecb
tortorse <tortorse@gmail.com>
2018-09-07 14:48:37 +0800

    perf: add suffix to less and modify prefix scope

--- src/components/nav-bar/nav-bar.vue
+++ src/components/nav-bar/nav-bar.vue
@@ -61,5 +61,5 @@ export default {
 }
 </script>
 <style lang="less">
-@import url('style/index.less');
+@import './style/index.less';
 </style>
--- src/components/progress/progress.vue
+++ src/components/progress/progress.vue
@@ -72,5 +72,5 @@ export default {
 </script>
 
 <style lang="less">
-@import './style/index';
+@import './style/index.less';
 </style>
