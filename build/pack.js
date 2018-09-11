cafd7c4c5f3b4da649a67408aebe8b93f7c3cf66
tortorse <tortorse@gmail.com>
2018-09-11 10:47:22 +0800

    perf: optimize the pack script

--- build/pack.js
+++ build/pack.js
@@ -1,19 +1,20 @@
+'use strict'
 require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
-var ora = require('ora')
-var rm = require('rimraf')
-var path = require('path')
-var chalk = require('chalk')
-var webpack = require('webpack')
-var config = require('../config')
-var webpackConfig = require('./webpack.pack.prod.conf')
+const ora = require('ora')
+const rm = require('rimraf')
+const path = require('path')
+const chalk = require('chalk')
+const webpack = require('webpack')
+const config = require('../config')
+const webpackConfig = require('./webpack.pack.prod.conf')
 
-var spinner = ora('building for production...')
+const spinner = ora('building for package...')
 spinner.start()
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+rm(path.join(config.build.assetsRoot), err => {
   if (err) throw err
   webpack(webpackConfig, function (err, stats) {
     spinner.stop()
